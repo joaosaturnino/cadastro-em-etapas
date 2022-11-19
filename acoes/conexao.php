1ea32ed05f5c64c31f5b155d9ef4ffaa467bf127
joaosaturnino <joao.saturnino@etec.sp.gov.br>
2022-11-19 11:57:34 -0300

    tes

--- acoes/conexao.php
+++ acoes/conexao.php
@@ -1,13 +1,13 @@
 <?php
-// $host = '127.0.0.1';
-// $usuario = 'root';
-// $senha = '';
-// $bd = 'buscateste3';
+$host = '127.0.0.1';
+$usuario = 'root';
+$senha = '';
+$bd = 'buscateste3';
 
-$host = '10.67.22.216';
-$usuario = 's221_tcc_g1_us';
-$senha = 'delv220809';
-$bd = 's221_tcc_g1_bd';
+// $host = '10.67.22.216';
+// $usuario = 's221_tcc_g1_us';
+// $senha = 'delv220809';
+// $bd = 's221_tcc_g1_bd';
 
     $conn = @mysqli_connect ($host, $usuario, $senha, $bd);
 
